d66c9aa2b4cf9fcc253412586dcac868a44c0ced
Chloë Strainge <nullptr.0@gmail.com>
2024-10-23 20:52:43 -0700

    Added constant words.

--- src/runtime/built_ins/base_words/constant_words.rs
+++ src/runtime/built_ins/base_words/constant_words.rs
@@ -1,8 +1,24 @@
 
-use crate::runtime::interpreter::Interpreter;
+use crate::{ add_native_word,
+             runtime::{ data_structures::value::{ ToValue, Value },
+             interpreter::Interpreter } };
 
 
 
-pub fn register_constant_words(_interpreter: &mut dyn Interpreter)
+pub fn register_constant_words(interpreter: &mut dyn Interpreter)
 {
+    add_native_word!(interpreter, "none",
+        |interpreter| { interpreter.push(&Value::None); Ok(()) },
+        "Push the value of none onto the data stack.",
+        " -- none");
+
+    add_native_word!(interpreter, "true",
+        |interpreter| { interpreter.push(&true.to_value()); Ok(()) },
+        "Push the value true onto the data stack.",
+        " -- true");
+
+   add_native_word!(interpreter, "false",
+       |interpreter| { interpreter.push(&false.to_value()); Ok(()) },
+       "Push the value false onto the data stack.",
+       " -- false");
 }
