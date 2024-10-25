eaab98c4e0b0cca882d0522a7a1cb7a9c1b7c23d
Chloë Strainge <nullptr.0@gmail.com>
2024-10-25 14:05:58 -0700

    Removed equality_words module.

--- src/runtime/built_ins/base_words/equality_words.rs
+++
@@ -1,8 +0,0 @@
-
-use crate::runtime::interpreter::Interpreter;
-
-
-
-pub fn register_equality_words(_interpreter: &mut dyn Interpreter)
-{
-}
--- src/runtime/built_ins/base_words/mod.rs
+++ src/runtime/built_ins/base_words/mod.rs
@@ -12,25 +12,23 @@ mod array_words;
 mod byte_buffer_words;
 mod hash_table_words;
 mod math_logic_and_bit_words;
-mod equality_words;
 
 
 
 use crate::runtime::{ built_ins::base_words::{
-                                        sorth_words::register_sorth_words,
-                                        stack_words::register_stack_words,
-                                        constant_words::register_constant_words,
-                                        bytecode_words::register_bytecode_words,
-                                        word_words::register_word_words,
-                                        word_creation_words::register_word_creation_words,
-                                        value_type_words::register_value_type_words,
-                                        string_words::register_string_words,
-                                        data_structure_words::register_data_structure_words,
-                                        array_words::register_array_words,
-                                        byte_buffer_words::register_byte_buffer_words,
-                                        hash_table_words::register_hash_table_words,
-                                        math_logic_and_bit_words::register_math_logic_and_bit_words,
-                                        equality_words::register_equality_words },
+                                      sorth_words::register_sorth_words,
+                                      stack_words::register_stack_words,
+                                      constant_words::register_constant_words,
+                                      bytecode_words::register_bytecode_words,
+                                      word_words::register_word_words,
+                                      word_creation_words::register_word_creation_words,
+                                      value_type_words::register_value_type_words,
+                                      string_words::register_string_words,
+                                      data_structure_words::register_data_structure_words,
+                                      array_words::register_array_words,
+                                      byte_buffer_words::register_byte_buffer_words,
+                                      hash_table_words::register_hash_table_words,
+                                      math_logic_and_bit_words::register_math_logic_and_bit_words },
                       interpreter::Interpreter};
 
 
@@ -50,5 +48,4 @@ pub fn register_base_words(interpreter: &mut dyn Interpreter)
     register_byte_buffer_words(interpreter);
     register_hash_table_words(interpreter);
     register_math_logic_and_bit_words(interpreter);
-    register_equality_words(interpreter);
 }
